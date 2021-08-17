5d505c7375f36d87f8ca508363a0d5adb01e5b29
Mario Finelli <mario@finel.li>
2021-08-17 17:43:45 -0400

    Add simple terraform wrapper

---
+++ terraform/terraform.bash
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+# simple wrapper around terraform to encrypt/decrypt the statefile
+# usage: ./terraform.bash
+
+if [[ $# -ne 0 ]]; then
+  echo >&2 "usage: $(basename "$0")"
+  exit 1
+fi
+
+if [[ -f terraform.tfstate.asc ]]; then
+  gpg -d terraform.tfstate.asc > terraform.tfstate
+
+  if [[ $? -ne 0 ]]; then
+    echo >&2 "error: unable to decrypt statefile"
+    exit 1
+  fi
+fi
+
+terraform apply
+tfret=$?
+
+# we always want to re-encrypt the statefile; terraform doesn't do rollbacks
+# so the state may have changed even though the apply failed
+gpg -ear 36FDA306 terraform.tfstate
+
+if [[ $? -ne 0 ]]; then
+  echo "error: statefile encryption failed!"
+  exit 25
+fi
+
+exit $tfret
