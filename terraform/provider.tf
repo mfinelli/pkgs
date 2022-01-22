58968cb32dc66eeace5ccc5643ca4a9eb94453eb
Mario Finelli <mario@finel.li>
2022-01-22 09:42:21 -0500

    Migrate state to terraform cloud

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -1,4 +1,13 @@
 terraform {
+  required_version = "~> 1.1"
+
+  cloud {
+    organization = "mfinelli"
+    workspaces {
+      name = "pkgs"
+    }
+  }
+
   required_providers {
     vultr = {
       source  = "vultr/vultr"
